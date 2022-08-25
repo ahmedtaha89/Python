22cbfcbc5cc70ca1c63c6f0e2b2169758a1064c3
ahmedtaha89 <95527363+ahmedtaha89@users.noreply.github.com>
2022-08-25 13:33:01 +0200

    Assignments Lesson(1-10)

---
+++ Assignments.py
@@ -0,0 +1,14 @@
+#Introduction And Variables
+#comment 
+#Data Type
+#Variables
+#escape space
+# => \    escape newline and escape backspace
+# => \\   escape backspace
+# => \n   newline
+# => \b   backspace
+# => \t   tap
+# => \'   escape single quote
+# => \"   escape double quote
+# => \r   current return
+# => \xhh  hex value
