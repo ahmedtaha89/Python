91915b6e3e017c7aedb48fc82d1693d0ee450bbe
ahmedtaha89 <95527363+ahmedtaha89@users.noreply.github.com>
2022-08-27 18:43:08 +0200

    modified

--- test.py
+++ test.py
@@ -214,3 +214,59 @@ Third""")
 myString = "Access Single Item"
 print(myString[::4])
 
+# ---------------------
+# -- Strings Methods --
+# ---------------------
+
+# strip() rstrip() lstrip()
+
+a = "    I Love Python    "
+print(a.strip())
+print(a.rstrip())
+print(a.lstrip())
+
+a = "#####I Love Python####"
+print(a.strip("#"))
+print(a.rstrip("#"))
+print(a.lstrip("#"))
+
+a = "@#@#@#I Love Python@#@#@#"
+print(a.strip("@#"))
+print(a.rstrip("@#"))
+print(a.lstrip("@#"))
+
+# title()
+
+b = "I Love 2d Graphics and 3g Technology and python"
+print(b.title())
+
+# capitalize()
+
+b = "I Love 2d Graphics and 3g Technology and python"
+print(b.capitalize())
+
+# zfill
+
+c, d, e, f = "1", "11", "111", "1111"
+
+print(c)
+print(d)
+print(e)
+print(f)
+
+print(c.zfill(4))
+print(d.zfill(4))
+print(e.zfill(4))
+print(f.zfill(4))
+
+# upper()
+
+g = "osama"
+
+print(g.upper())
+
+# lower()
+
+h = "OSama"
+
+print(h.lower())
