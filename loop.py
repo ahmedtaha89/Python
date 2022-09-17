8d9e96bd69d3466eb7a85bb590de3d02fe211137
ahmedtaha89 <95527363+ahmedtaha89@users.noreply.github.com>
2022-09-17 22:56:45 +0200

    loop

---
+++ loop.py
@@ -0,0 +1,15 @@
+# F_loop = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# for loop in F_loop:
+#     if loop % 2 == 0:
+#         print(f"Even:{loop}")
+#     elif loop % 2 != 0:
+#         print(f"Odd:{loop}")
+# else :
+#     print("Done")
+
+t = 'ahmed taha'
+a = 0
+for test in t:
+    a+=1
+    print(f"({a}){test}*2".strip().capitalize())
--- test.py
+++ test.py
@@ -1902,3 +1902,26 @@ while enter_pass != password:
 
 else:
     print("Password correct")
+
+# -----------------
+# -- Loop => For --
+# -----------------
+# for item in iterable_object :
+#   Do Something With Item
+# -----------------------------
+# item Is A Vairable You Create and Call Whenever You Want
+# item refer to the current position and will run and visit all items to the end
+# iterable_object => Sequence [ list, tuples, set, dict, string of charcaters, etc ... ]
+# ---------------------------------------------------------------
+
+
+
+F_loop = [1,2,3,4,5,6,7,8,9,10]
+
+for  loop in F_loop :
+    print(loop)
+    
+# -----------------
+# -- Loop => For --
+# --  Trainings  --
+# -----------------
