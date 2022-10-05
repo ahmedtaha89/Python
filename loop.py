87a2e7c870a71c62191a2360610f3fb58b8d5970
AhmedTaha74 <95527363+ahmedtaha89@users.noreply.github.com>
2022-10-05 08:13:31 +0200

    modified

--- loop.py
+++ loop.py
@@ -11,6 +11,45 @@ print(my_all(1,2,3,54,0,()))
 
 
  
- 
+ # ------------------------
+# -- Built In Functions --
+# ------------------------
+# abs() => | |  => القيمه المطلقه => return positive number
+# pow() => power
+# min()
+# max()
+# slice()
+# ------------------------
+
+
+a = -100
+b = -0.54
+c = 5
+print(abs(a))
+print(abs(b))
+print(abs(c))
+
+# pow(base, exp, mod || %)
+print(pow(2,2,2))
+
+
+# min(item, item , item, or iterator) => اقل قيمه 
+test_min = [84,89,448,4,77,-1,44] 
+print(min(test_min))
+
+# max(item, item , item, or iterator) => اكبر قيمه 
+myNumbers = (1, 20, -50, -100, 100)
+print(max(myNumbers))
+
+# not include end
+# slice(start, end, step) => لازم تحدد النهايه
+
+a = ["A", "B", "C", "D", "E", "F"]
+print(a[:-1])
+print(a[2:-1])
+print(a[0:])
+
+
+print(a[slice(2,-1)])
  
  
