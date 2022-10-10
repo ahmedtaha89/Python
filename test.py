3df23bb354ee3a9a6d253ae151d522f2963c1d9a
ahmedtaha89 <ahmed_947a@fci.kfs.edu.eg>
2022-10-10 13:38:26 +0200

    Iterable and Iterator

--- test.py
+++ test.py
@@ -2690,9 +2690,57 @@ print(datetime.datetime.now().strftime("%c"))
 print(datetime.datetime.now().strftime("Time Now : %X"))
 
 
+# --------------------------
+# -- Iterable vs Iterator --
+# --------------------------
+# Iterable
+# [1] Object Contains Data That Can Be Iterated Upon
+# [2] Examples (String, List, Set, Tuple, Dictionary)
+# ------------------------------------------
+# Iterator
+# [1] Object Used To Iterate Over Iterable Using next() Method Return 1 Element At A Time
+# [2] You Can Generate Iterator From Iterable When Using iter() Method
+# [3] For Loop Already Calls iter() Method on The Iterable Behind The Scene
+# [4] Gives "StopIteration" If Theres No Next Element
+# -----------------------------------------------------------
+
+myString = "Osama"
+
+myList = [1, 2, 3, 4, 5]
+
+for letter in myString:
+
+  print(letter, end=" ")
+
+for number in myList:
+
+  print(number, end=" ")
+
+myIterator = iter(myString)
+
+print(next(myIterator))
+print(next(myIterator))
+print(next(myIterator))
+print(next(myIterator))
+print(next(myIterator))
+print(next(myIterator))
+
+for letter in iter("Elzero"):
+
+  print(letter, end=" ")
+
+ 
+# iterable  => loop اقدر اعمل عليه  object  اي 
 
 
+My_List = [1,2,3,4,5]
 
+for l in My_List :
+    
+    print(l ,end="\t \t")
+    
+name = "ahmed"
 
-
-# 
+for n in name:
+    
+    print(name)    
