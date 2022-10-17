aca37dc598016dacc53743e69b021c5b3c2a29c0
ahmedtaha89 <ahmed_947a@fci.kfs.edu.eg>
2022-10-17 20:35:57 +0200

    modified

--- loop.py
+++ loop.py
@@ -1,1 +1,51 @@
+# number_tries = 4
+# the_file = None
 
+# while number_tries > 0 :
+    
+#     try:
+#         print("Enter The File Name With Absolute Path To Open")
+#         print(f"You have {number_tries} tries.")
+#         print("EX:F:\Test virtual\Project")
+        
+#         input_file = str(input())
+        
+#         the_file = open(input_file,"r")
+        
+#         print(the_file.read())
+#     except:
+#         print("the path is not correct try again.")
+#         number_tries-=1
+        
+#     finally:
+       
+#      if the_file is not None:
+
+#         the_file.close()
+# else : 
+#     print("All try finished")
+
+# from turtle import * 
+# color("red")
+# begin_fill()
+# pensize(3)
+# left(50)
+# forward(133)
+# circle(50,200)
+# right(140)
+# circle(50,200)
+# end_fill()
+
+
+
+
+NUM = input("Add Your Number ")
+if len(NUM) == 1 :
+    
+        print("####################")
+        print(f"The Number Is {NUM}")
+        print("####################")
+  
+          
+raise IndexError ("Only One Character Allowed") 
+raise 
